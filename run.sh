0b5c724813bad1b29263b7b1b7fde014608bac98
naore <feyy6326@hotmail.com>
2019-03-13 00:01:12 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 /usr/sbin/sshd -D &
-ss-server -c /etc/shadowsocks-libev/config.json &
+ss-server -c /etc/shadowsocks-libev/config.json -f /s.pid &
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json &
