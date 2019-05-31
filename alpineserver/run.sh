5df4445d6fe943e56a7f3ad9db93cb083dadfa93
naore <feyy6326@hotmail.com>
2019-05-31 19:05:07 +0800

    Update run.sh

--- alpineserver/run.sh
+++ alpineserver/run.sh
@@ -1,3 +1,1 @@
-/usr/sbin/sshd -D && \
-/usr/bin/ss-server -c /etc/shadowsocks-libev/config.json -f /s.pid && \
-/usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json 
+"/usr/sbin/sshd -D && /usr/bin/ss-server -c /etc/shadowsocks-libev/config.json -f /s.pid && /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json"
